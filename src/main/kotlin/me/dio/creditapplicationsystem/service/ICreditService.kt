540ce5af5c7b06a080c4a2c335c1a33399be57e2
Enoque Neres <enoque.sneres@live.com>
2024-03-13 23:55:43 -0300

    camada service

---
+++ src/main/kotlin/me/dio/creditapplicationsystem/service/ICreditService.kt
@@ -0,0 +1,13 @@
+package me.dio.creditapplicationsystem.service
+
+import me.dio.creditapplicationsystem.entity.Credit
+import org.hibernate.validator.constraints.UUID
+
+interface ICreditService {
+
+    fun save(credit: Credit): Credit
+
+    fun findAllByCustomer(customerId: Long): List<Credit>
+
+    fun findByCreditCode(customerId: Long, creditCode: UUID): Credit
+}
