768ff273c9657bdce672914c4a67ed69ff9e7858
Enoque Neres <enoque.sneres@live.com>
2024-03-25 02:26:58 -0300

    correções

--- src/main/resources/db/migration/V2__create_table_credit.sql
+++ src/main/resources/db/migration/V2__create_table_credit.sql
@@ -4,11 +4,11 @@ CREATE TABLE credit (
    credit_value DECIMAL NOT NULL,
    day_first_installment date NOT NULL,
    number_of_installments INT NOT NULL,
-   status SMALLINT,
+   status INT,
    customer_id BIGINT,
    CONSTRAINT pk_credit PRIMARY KEY (id)
 );
 
-ALTER TABLE credit ADD CONSTRAINT uc_credit_credit_code UNIQUE (credit_code);
+ALTER TABLE credit ADD CONSTRAINT uc_credit_creditcode UNIQUE (credit_code);
 
 ALTER TABLE credit ADD CONSTRAINT FK_CREDIT_ON_CUSTOMER FOREIGN KEY (customer_id) REFERENCES customer (id);
